83d195dc758c26cc9007706aac2cb46cd2bdd17b
Jason Zhang <jason@jlmtalent.com>
2017-12-13 11:49:05 -0800

    testing on one resume: Angie Yuan. only inputs name, work exp w/ no description, and education

--- info_data.py
+++ info_data.py
@@ -1,10 +1,6 @@
 import json
 from pprint import pprint
-
-JSON_DIR = './json'
-
-with open(JSON_DIR + '/' + 'Angie_Yuan.json') as json_file:
-    data = json.load(json_file)
+from inject_tex import create_file
 
 # pprint(data)
 # print(data)
@@ -33,3 +29,37 @@ with open(JSON_DIR + '/' + 'Angie_Yuan.json') as json_file:
 # TODO: How to reformat skills into short words or sentences
 # print(data['Resume']['StructuredXMLResume']['Qualifications']['QualificationSummary'])
 
+JSON_DIR = './json'
+
+with open(JSON_DIR + '/' + 'Angie_Yuan.json') as json_file:
+    data = json.load(json_file)
+
+useful = data['Resume']['StructuredXMLResume']
+
+
+person_name = str(useful['ContactInfo']['PersonName']['FormattedName'])
+
+school_list = []
+school_sec = useful['EducationHistory']['SchoolOrInstitution']
+for school in school_sec:
+    school_name = str(school['School']['SchoolName'])
+    school_start = str(school['Degree']['DatesOfAttendance']['StartDate']['YearMonth'])
+    school_end = str(school['Degree']['DatesOfAttendance']['EndDate']['YearMonth'])
+    school_degree = str(school['Degree']['DegreeName'])
+    school_list.append([school_name, school_start, school_end, school_degree])
+
+job_list = []
+job_sec = useful['EmploymentHistory']['EmployerOrg']
+for job in job_sec:
+    job_info = job['PositionHistory'][0]
+    job_org = str(job_info['OrgName']['OrganizationName'])
+    job_start = str(job_info['StartDate']['YearMonth'])
+    job_end = str(job_info['EndDate']['YearMonth'])
+    job_title = str(job_info['Title'])
+    # job_description = useful['EmploymentHistory']['EmployerOrg'][0]['PositionHistory'][0]['Description']
+    job_description = []
+    job_list.append([job_org, job_start, job_end, job_title, job_description])
+
+
+file_name = person_name.replace(' ', '-') + '_JLM'
+create_file(file_name, person_name, [], job_list, school_list)
